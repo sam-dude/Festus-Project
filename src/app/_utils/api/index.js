e88d52bd9ce324cb41aff96200df1bf194c07e1e
sam-dude <blendert5@gmail.com>
2024-08-08 12:54:13 +0100

    fix

--- src/app/_utils/api/index.js
+++ src/app/_utils/api/index.js
@@ -5,8 +5,13 @@ const ESP32_IP_ADDRESS = process.env.NEXT_PUBLIC_ESP32_IP_ADDRESS
 
 const useApiCall = () => {
     const get = async (url) => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
         try {
-            const response = await axios.get(`http://${ESP32_IP_ADDRESS}/${url}`)
+            const response = await axios.get(`http://${ESP32_IP_ADDRESS}/${url}`, config)
         return { data: response.data, status: response.status }
         } catch (error) {
             console.error(error)
